38073c453e58dfb1668ac2e5f61dc0b0bb28cd93
郑逢时 <962907278@qq.com>
2020-03-20 17:32:06 +0800

    [add] weight visualizer

--- filter.py
+++ filter.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 from optimizer import *
 from multiprocessing import cpu_count
+from matplotlib import pyplot as plt
 
 EPSILON = 1e-5
 
@@ -77,6 +78,33 @@ def denoise(im_mean: np.ndarray, im_var: np.ndarray, im_original: np.ndarray, fi
 	return im_result
 
 
+def compute_filter(im_mean, im_var, filter_size: tuple, at: tuple, **kwargs):
+	im_mean, im_var = check_shape(im_mean), check_shape(im_var)
+	assert im_mean.shape == im_var.shape
+	n_row, n_col = im_mean.shape[0:2]
+	f_r, f_c = filter_size[0:2]
+	if not (f_r & 1 and f_c & 1):
+		raise ValueError('Bad filter size, expect odd size!')
+	f_r_2, f_c_2 = int(f_r / 2), int(f_c / 2)
+	i_r, i_c = at
+	# filter pixel i using neighbors j:
+	j_r_begin = max(0, i_r - f_r_2)
+	j_r_end = min(n_row, i_r + f_r_2 + 1)
+	j_c_begin = max(0, i_c - f_c_2)
+	j_c_end = min(n_col, i_c + f_c_2 + 1)
+	b_multi = im_mean[j_r_begin: j_r_end, j_c_begin: j_c_end, :] - im_mean[i_r, i_c, :]
+	v_multi = im_var[j_r_begin: j_r_end, j_c_begin: j_c_end, :]
+	w_multi = np.zeros_like(b_multi)
+	# compute filter for each channel:
+	for channel in range(3):
+		b = b_multi[:, :, channel].flatten()
+		v = v_multi[:, :, channel].flatten() + EPSILON
+		assert (v > 0).all()
+		w = opt_GCG(b, v, ret_steps=False, **kwargs)
+		w_multi[:, :, channel] = w.reshape((j_r_end - j_r_begin, j_c_end - j_c_begin))
+	return w_multi
+
+
 def imread(path) -> np.ndarray:
 	return np.array(Image.open(path), dtype='float64') / 255.
 
@@ -86,13 +114,33 @@ def imwrite(img: np.ndarray, path):
 	Image.fromarray((255 * img).astype('uint8')).save(path)
 
 
-def imshow(img: np.ndarray):
+def imshow(img: np.ndarray, title=None):
 	img[img > 1.0] = 1.0
-	Image.fromarray((255 * img).astype('uint8')).show()
+	Image.fromarray((255 * img).astype('uint8')).show(title)
 
 
 def show_diff(img1, img2):
 	imshow(img1 - img2)
+
+
+def visualize_weight(im_mean, im_var, im_background, filter_size: tuple, at: tuple, gamma=0.1, alpha=1.0,
+					 in_place=False, **kwargs):
+	weight = compute_filter(im_mean, im_var, filter_size, at, **kwargs).mean(axis=2) + EPSILON
+	weight = weight ** gamma
+	color_map = plt.get_cmap('afmhot')
+	colored_weight = check_shape(color_map(weight))
+
+	n_row, n_col = im_mean.shape[0:2]
+	f_r_2, f_c_2 = int(filter_size[0] / 2), int(filter_size[1] / 2)
+	i_r, i_c = at
+	j_r_begin = max(0, i_r - f_r_2)
+	j_r_end = min(n_row, i_r + f_r_2 + 1)
+	j_c_begin = max(0, i_c - f_c_2)
+	j_c_end = min(n_col, i_c + f_c_2 + 1)
+	im = im_background if in_place else im_background.copy()
+	im[j_r_begin:j_r_end, j_c_begin:j_c_end] = \
+		alpha * colored_weight + (1 - alpha) * im[j_r_begin:j_r_end, j_c_begin:j_c_end]
+	return im
 
 
 # small test:
@@ -104,14 +152,21 @@ def show_diff(img1, img2):
 # im_result = denoise(im_mean, im_var, im_original, (63, 63), max_iter=0)
 # imwrite(im_result, 'out/small 63x63/filtered_iter_0_bias_var_guided_by_self.png')
 
-# massive test
+# massive test:
 im_mean = imread('data/images-png-part1/16spp/mean_cross_filtered_iter2.png')
 im_var = imread('data/images-png-part1/16spp/variance_cross_filtered_iter2.png') / (16 ** 2)
-im_original = imread('data/16mean.png')
+im_original = check_shape(imread('data/16mean.png'))
+# visualize filter weight:
+points = [(230, 520), (358, 367), (446, 552), (470, 181), (386, 181), (310, 636), (552, 826)]
+for point in points:
+	visualize_weight(im_mean, im_var, im_original, (63, 63), at=point, alpha=1, gamma=0.1, in_place=True, max_iter=500)
+imwrite(im_original, 'out/full 63x63/weight_iter_500.png')
+# draw weights on im_original
+
 # imwrite(im_original, 'out/full 63x63/16original.png')
 # imwrite(im_mean, 'out/full 63x63/mean_cross_filtered_iter2.png')
-im_result = denoise(im_mean, im_var, im_original, (63, 63), max_iter=0)
-imwrite(im_result, 'out/full 63x63/iter_0_init_bias_var_guided_by_cross++++.png')
+# im_result = denoise(im_mean, im_var, im_original, (63, 63), max_iter=0)
+# imwrite(im_result, 'out/full 63x63/iter_0_init_bias_var_guided_by_cross++++.png')
 
 # diff
 # imwrite(imread('out/full 63x63/16reference.png')[:,:,0:3] - imread('out/full 63x63/iter_0_init_bias_var.png'), 'out/full 63x63/diff.png')
